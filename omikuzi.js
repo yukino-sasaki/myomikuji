347494876f6d7ee778fe1238403cc1e114eac3c6
yukino-sasaki <yukisnowman.18@gmail.com>
2021-02-25 01:31:32 +0900

    一応動くようになったおみくじプログラムです

--- omikuzi.js
+++ omikuzi.js
@@ -1,29 +1,29 @@
-/*
-var clickbutton = document.querySelector('omi-btn')
-var result = document.querySelector('result');
+
+var clickbutton = document.querySelector('.omi-btn')
+var result = document.querySelector('.result');
 var newresult = document.createElement('p');
-var a = 0;
+
 clickbutton.addEventListener('click', () => {
 
     var a = Math.floor(Math.random() * 4);
-    return a;
-});
-if (a === 0) {
-    newresult.textContent = "大吉";
-    result.appendChild(newresult);
-} else if (a === 1) {
 
-    newresult.textContent = "中吉";
-    result.appendChild(newresult);
-} else if (a === 2) {
-    newresult.textContent = "小吉";
-    result.appendChild(newresult);
-} else {
-    newresult.textContent = "凶";
-    result.appendChild(newresult);
-}
-*/
-window.onload = function () {
+    if (a === 0) {
+        newresult.textContent = "大吉";
+        result.appendChild(newresult);
+    } else if (a === 1) {
+
+        newresult.textContent = "中吉";
+        result.appendChild(newresult);
+    } else if (a === 2) {
+        newresult.textContent = "小吉";
+        result.appendChild(newresult);
+    } else {
+        newresult.textContent = "凶";
+        result.appendChild(newresult);
+    }
+});
+
+/*
     var items = ["大吉", "中吉", "小吉", "凶"];
     var btn = document.querySelector('omi-btn');
     var result = document.querySelector("result");
@@ -32,4 +32,5 @@ window.onload = function () {
         var num = Math.floor(Math.random() * items.length);
         alert(num);
     })
-}
+
+    */
